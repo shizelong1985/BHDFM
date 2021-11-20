dada8811b592fbf57a55202702e3eacbc9c10b52
Anthony M. Thomas, Jr <amthomasjr@gmail.com>
2021-11-19 21:32:41 -0600

    Create pc_T.R

---
+++ code/_functions/pc_T.R
@@ -0,0 +1,42 @@
+#---------------------------------------------------------------------------------------------------
+#   This function estimates k latent common factors based on a panel of variables using the method 
+#   of principal components. It is assumed that the panel can be described by a 
+#   factor model of the form 
+#           
+#           X = F * Lambda' + e
+#           
+#   This function relies on the identifying normalization that F'F/T = I.
+#   
+#   Inputs:
+#   --------------------------------------------- 
+#       x       = T by N matrix of data
+#       k       = number of factors to estimate
+#   
+#   Outputs:
+#   --------------------------------------------- 
+#       fhat    = matrix of estimated factors (T by K)
+#       lambda  = matrix of estimated factor loadings 
+#       ehat    = residuals
+# 
+#---------------------------------------------------------------------------------------------------
+
+pc_T <- function (x, k) {
+    
+    # Setting up preliminaries
+    output  <- list()
+    bigT    <- nrow(x)
+    
+    # SVD of Sum of Squares and Cross Products matrix
+    xx      <- x %*% t(x)   # T by T matrix
+    fhat0   <- svd(xx)$u    # T by T matrix
+    
+    # Estimating factors, loadings, and errors
+    fhat     <- as.matrix(fhat0[, 1:k]) * sqrt(bigT) # T by k matrix    
+    lambda   <- t(x) %*% fhat / bigT                 # N by k matrix
+    ehat     <- x - fhat %*% t(lambda)               # T by N matrix
+    
+    output$fhat     <- fhat
+    output$lambda   <- lambda
+    output$ehat     <- ehat
+    return(output)
+}
