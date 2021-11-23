be72ad6195a746ab5aeee3642d725af03d663fcc
Anthony M. Thomas, Jr <amthomasjr@gmail.com>
2021-11-23 02:56:06 -0600

    Update draw_psi_sigma.R

--- code/_functions/draw_psi_sigma.R
+++ code/_functions/draw_psi_sigma.R
@@ -7,8 +7,8 @@
 #   
 #   Outputs:
 #   --------------------------------------------- 
-#       psi   = vector of length K*q consisting of autoregressive coefficients      
-#       sigma = 
+#       psi   = K by q matrix of autoregressive coefficients      
+#       sigma = K by 1 matrix of variances
 #   
 #---------------------------------------------------------------------------------------------------
 
@@ -23,40 +23,41 @@ draw_psi_sigma <- function (Ft, psi_F, sig2_F, prior, params, var_fix, ...) {
     max_l_F <- max(l_F)
     output <- list()
     
-    PSI <- psi_F  # K_F by 1
-    SIG <- sig2_F # K_F by 1
+    PSI <- psi_F  
+    SIG <- sig2_F 
     
     for (k in 1:K_F) {
         q_Fk <- q_F[k]
-        xpsi_F1 <- matrix(0, nrow = max_q_F, 1)
+        xpsi_F1 <- matrix(0, nrow = max_q_F, ncol = 1)
         
         if (q_Fk > 0) {
-            ystar <- as.matrix(Ft[, k]) # bigT by K_F
+            ystar <- as.matrix(Ft[, k]) # bigT by 1
             xstar <- NA
-            
             for (i in 1:q_Fk) {
-                xstar <- cbind(xstar, lagn(Ft[, k], i)) # bigT by K_F
-            }
-            ystar <- trimr(ystar, max(max_q_F, max_l_F), 0) # (bigT - max) by K_F
-            xstar <- trimr(xstar, max(max_q_F, max_l_F), 0) # (bigT - max) by K_F
+                xstar <- cbind(xstar, lagn(Ft[, k], i)) 
+                xstar <- as.matrix(xstar[, -1])
+            } # dimension of xstar after the loop is bigT by q_Fk
+            
+            ystar <- trimr(ystar, max(max_q_F, max_l_F), 0) # (bigT - max) by 1
+            xstar <- trimr(xstar, max(max_q_F, max_l_F), 0) # (bigT - max) by q_Fk
             TT <- nrow(ystar)
             
             # bring to right dimension
-            prior_mean <- matrix(getElement(prior$Psi, "mean"), nrow = q_Fk, ncol = 1) # q_Fk by 1
+            prior_mean <- matrix(getElement(prior$Psi, "mean"), nrow = q_Fk, ncol = 1) 
             inv_prior_var <- diag(1 / getElement(prior$Psi, "var"), nrow = q_Fk) # q_Fk by q_Fk
             sig_inv <- 1/sig2_F[k] # scalar
             
-            # posterior mean and variance
-            # Dimension of post_var: either q_Fk by q_Fk or K_F by K_F
-            # Dimension of post_mean: either 
-            post_var <- solve( inv_prior_var + (sig_inv * t(xstar) %*% xstar) ) # 
-            post_mean <- post_var %*% (inv_prior_var %*% prior_mean + sig_inv * t(xstar) %*% ystar)
+            # Dimension of post_var: q_Fk by q_Fk
+            # Dimension of post_mean: q_Fk by 1
+            post_var <- solve( inv_prior_var + (sig_inv * t(xstar) %*% xstar) ) # q_Fk by q_Fk
+            post_mean <- post_var %*% (inv_prior_var %*% prior_mean + 
+                                           (sig_inv * t(xstar) %*% ystar))
             
             # draw from multivariate normal with posterior mean and variance
-            C <- chol(sig2_F[k] * post_var)
+            C <- chol(sig2_F[k] * post_var) # 
             accept <- 0; counter <- 0
             while (accept == 0 & counter < 1e3) {
-                psi_F1 <- post_mean + t(C) * rnorm(q_Fk, 1)
+                psi_F1 <- post_mean + t(C) %*% rnorm(q_Fk, 1)
                 a <- rbind(1, -psi_F1); ceof <- a[nrow(a):1, ]
                 root <- polyroot(ceof)
                 rootmod <- abs(root)
